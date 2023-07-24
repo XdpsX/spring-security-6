5e24a66a33031a148a7fa7c342064c89069bbd6f
Night <dranhata@gmail.com>
2023-07-24 13:42:32 +0700

    #4 extract claims

--- src/main/java/com/spring/security/config/JwtService.java
+++ src/main/java/com/spring/security/config/JwtService.java
@@ -1,12 +1,35 @@
 package com.spring.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
 
+    private static final String SECRET_KEY="832a6e0c4d2a76f095993cae332e8f47536d6ceb44eb94affed6d7743ed08597";
+
     public String extractUsername(String token) {
 
         return null;
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
